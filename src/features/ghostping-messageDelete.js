a8b3541d8b117dbf3aeebf526ac8a235811fa0a0
ThatsLiamS <73022526+ThatsLiamS@users.noreply.github.com>
2021-05-06 22:29:23 +0100

    Update ghostping-messageDelete.js

--- src/features/ghostping-messageDelete.js
+++ src/features/ghostping-messageDelete.js
@@ -1,9 +1,8 @@
 const Discord = require("discord.js");
 module.exports = {
     name: "ghostping-messageDelete",
-    description: ``,
     async execute(ErrorMessages, message, value) {
-        if (message === undefined) return console.log(ErrorMessages.MessageOne);
+        if (message === undefined) throw ErrorMessages.MessageOne
         if(message.author.bot || message.mentions.members.size == 0 && message.mentions.roles.size === 0) return 
 
         let embedInfo = { title: `Ghost Ping Detected`, color: `C0C0C0`, picture: `https://cdn.glitch.com/c7f57949-b55c-47c7-bd83-e7563dda5a78%2Fghost%20ping.png?v=1616338530868`, footer: `Don't Ghost Ping, smh`, channel: message.channel }
@@ -19,12 +18,15 @@ module.exports = {
             if(value.footer) embedInfo.footer = value.footer;
             if(value.channel) embedInfo.channel = await message.guild.channels.cache.get(value.channel)
 
-            if (!embedInfo.channel instanceof Discord.TextChannel) return console.log(`\ndiscordjs-ghost-ping: channel not found from ID provided\n\n'channel: ${value.channel}'`);
+            if (!embedInfo.channel instanceof Discord.TextChannel) throw ErrorMessages.unableToGetChannel
         } 
+
         if(stringMentions == "") return
 
-        const embed = new Discord.MessageEmbed().setTitle(`${embedInfo.title}`).setAuthor(`${message.member.user.tag}`, `${message.member.user.displayAvatarURL()}`).setColor(`${embedInfo.color}`).setThumbnail(`${embedInfo.picture}`).setDescription(`**Author:** ${message.author}\n**Channel:** ${message.channel}\n\n**Mentions:**\n${stringMentions}`).setFooter(`${embedInfo.footer}`).setTimestamp();
-        embedInfo.channel.send({embed})
+        const embed = new Discord.MessageEmbed().setTitle(`${embedInfo.title}`).setAuthor(`${message.member.user.tag}`, `${message.member.user.displayAvatarURL()}`).setColor(`${embedInfo.color}`).setThumbnail(`${embedInfo.picture}`).addFields( { name: `**Channel:**`, value: `${message.channel}`, inline: true }, { name: `**Mentions:**`, value: `${stringMentions}`, inline: true }).setFooter(`${embedInfo.footer}`).setTimestamp();
+        embedInfo.channel.send({embed}).catch((error) => { 
+            throw ErrorMessages.unableToSendMessage
+        })
 
         return true
     }
