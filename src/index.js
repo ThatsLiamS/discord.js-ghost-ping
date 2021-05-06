2a0d753b7ad63072b578ed050b1f97aa9cf65dc8
ThatsLiamS <73022526+ThatsLiamS@users.noreply.github.com>
2021-05-06 22:28:48 +0100

    Created custom Error Messages

--- src/index.js
+++ src/index.js
@@ -8,19 +8,37 @@ for(const file of featureFiles){
     files.set(fileInfo.name, fileInfo);
 }
 
+class ParameterError extends Error {
+    constructor(...params) {
+        super(...params)
+        this.name = "ParameterError"  
+    }
+}
+
+class MissingPermissions extends Error {
+    constructor(...params) {
+        super(...params)
+        this.name = "MissingPermissions"  
+    }
+}
+
 const ErrorMessages = {
-    expectedParameterError: `\ndiscordjs-ghost-ping: expected parameter (EventType) in dectector()\n\ndiscord.js#client.on(\"messageDelete\", message => {\n                       ^\n                       |\n(EventType) is \'messageDelete\'\n`,
-    unpectedParameterError: `\ndiscordjs-ghost-ping: unexpected parameter (EventType) in dectector()\n\ndiscord.js#client.on(\"messageDelete\", message => {\n                       ^\n                       |\n(EventType) is \'messageDelete\'\n`,
-    MessageOne: `\ndiscordjs-ghost-ping: expected parameter (message) in dectector()\n\ndiscord.js#client.on(\"messageDelete\", message => {\n                                        ^\n                                        |\n(message) is \'message\'`,
-    MessageBoth: `\ndiscordjs-ghost-ping: expected parameters (oldMessage, newMessage) in ghostPing#dectector()\n\ndiscord.js#client.on(\"messageUpdate\", oldMessage, newMessage => {\n                                        ^\n                                        |\n(oldMessage, newMessage) is \'oldMessage, newMessage\'`,
+    unableToSendMessage: new MissingPermissions(`Unable to send message to channel provided`),
+    
+    expectedParameterError: new ParameterError(`Expected parameter (EventType) in dectector()\n\nclient.on(\"messageDelete\", message => {\n                       ^\n                       |\n(EventType) is \'messageDelete\'\n`),
+    unpectedParameterError: new ParameterError(`Unexpected parameter (EventType) in dectector()\n\nclient.on(\"messageDelete\", message => {\n                       ^\n                       |\n(EventType) is \'messageDelete\'\n`),
+    MessageOne: new ParameterError(`Expected parameter (message) in dectector()\n\nclient.on(\"messageDelete\", message => {\n                           ^\n                           |\n(message) is \'message\'`),
+    MessageBoth: new ParameterError(`Expected parameters (oldMessage, newMessage) in dectector()\n\nclient.on(\"messageUpdate\", oldMessage, newMessage => {\n                                        ^\n                                        |\n(oldMessage, newMessage) is \'oldMessage, newMessage\'`),
+    unableToGetChannel: new ParameterError(`Invalid channel ID provided, unable to get channel`)
 }
 
 module.exports = {
-     detector: (eventType, two, three, four) => {
-        if (eventType === undefined) return console.log(ErrorMessages.expectedParameterError)
+    detector: (eventType, two, three, four) => {
+        if (eventType === undefined) throw ErrorMessages.expectedParameterError
 
         if(eventType == "messageDelete") return files.get('ghostping-messageDelete').execute(ErrorMessages, two, three)
         if(eventType == "messageUpdate") return files.get('ghostping-messageUpdate').execute(ErrorMessages, two, three, four)
-        else console.log(ErrorMessages.unpectedParameterError)
+        
+        else throw ErrorMessages.unpectedParameterError
     }
 }
