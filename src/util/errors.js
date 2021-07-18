81e8336c5a421316787d7e3dcd9e5db2fc053591
Liam Skinner <liamdavidskinner@gmail.com>
2021-07-18 22:49:16 +0100

    Removed useless error message
    Fixed error handling, message became uneeded by the default

--- src/util/errors.js
+++ src/util/errors.js
@@ -12,13 +12,6 @@ class MissingPermissions extends Error {
 	}
 }
 
-class ChannelNotFound extends Error {
-	constructor(...params) {
-		super(...params);
-		this.name = "ChannelNotFound";
-	}
-}
-
 const ErrorMessages = {
 	unableToSendMessage: new MissingPermissions(`Unable to send message to channel provided`),
 
@@ -26,8 +19,6 @@ const ErrorMessages = {
 	unpectedParameterError: new ParameterError(`Unexpected parameter (EventType) in dectector()\n\nclient.on("messageDelete", message => {\n                       ^\n                       |\n(EventType) is 'messageDelete'\n`),
 	MessageOne: new ParameterError(`Expected parameter (message) in dectector()\n\nclient.on("messageDelete", message => {n                           ^\n                           |\n(message) is 'message'`),
 	MessageBoth: new ParameterError(`Expected parameters (oldMessage, newMessage) in dectector()\n\nclient.on("messageUpdate", oldMessage, newMessage => {\n                                        ^\n                                        |\n(oldMessage, newMessage) is 'oldMessage, newMessage'`),
-
-	unableToGetChannel: new ChannelNotFound(`Invalid channel ID provided.`),
 };
 
 module.exports = {
