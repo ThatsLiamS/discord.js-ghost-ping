7bddbf817d835e868448ff73413bf4e27aadcd82
Liam Skinner <liamdavidskinner@gmail.com>
2021-07-18 23:00:43 +0100

    combined files and updated syntax
    - Combined send and embedValues
    - Added usage of ternary operators for customisation
    - Added string validation
    - Updated send() to match new specs

--- src/util/ignore.js
+++ src/util/ignore.js
@@ -1,20 +1,10 @@
-function perms(member, array) {
-	let result = false;
-
-	/* Checks if the array exists */
-	if(!array || array === [] || (array instanceof Array == false)) { return result; }
-
-	/* Loops through all permissions and checks them */
-	array.forEach((permission) => {
-		if(member.hasPermission(permission)) {
-			result = true;
-		}
-	});
-
-	/* returns a boolean */
-	return result;
-}
-
 module.exports = {
-	perms
+	ignore: async (object, message) => {
+
+		let result = false;
+
+
+		return result;
+
+	}
 };
--- src/util/send.js
+++ src/util/send.js
@@ -1,34 +1,54 @@
 const Discord = require('discord.js');
-const { ErrorMessages } = require(`${__dirname}/../util/errors`);
-const { embedValues } = require(`${__dirname}/embedValues`);
 
-async function send(value, message, mentions) {
+const { ErrorMessages } = require(`${__dirname}/errors`);
+const { ignore } = require(`${__dirname}/ignore`);
 
-	const { title, color, picture, footer, channel } = embedValues(value, message);
+const custom = (object, message) => {
 
-	const embed = new Discord.MessageEmbed()
-		.setTitle(`${title}`)
-		.setAuthor(`${message.member.user.tag}`, `${message.member.user.displayAvatarURL()}`)
-		.setColor(`${color}`)
-		.setThumbnail(`${picture}`)
-		.addFields(
-			{ name: `**Channel:**`, value: `${message.channel}`, inline: true },
-			{ name: `**Mentions:**`, value: `${mentions}`, inline: true }
-		)
-		.setFooter(`${footer}`)
-		.setTimestamp();
+	const editedValues = {};
 
-	if (channel instanceof Discord.TextChannel == false && channel instanceof Discord.NewsChannel == false) {
-		throw ErrorMessages.unableToGetChannel;
+	editedValues.title = (object && object.title) ? object.title : 'Ghost Ping Detected';
+	if(editedValues.title.length > 256) { editedValues.title = 'Ghost Ping Detected'; }
+
+	editedValues.color = (object && object.color) ? object.color : 'C0C0C0';
+	if(editedValues.color.length > 50) { editedValues.color = 'C0C0C0'; }
+
+	editedValues.footer = (object && object.footer) ? object.footer : 'Don\'t Ghost Ping, smh';
+	if(editedValues.footer.length > 256) { editedValues.footer = 'Don\'t Ghost Ping, smh'; }
+
+	let channel;
+	if(object && object.channel) {
+		channel = message.guild.channels.cache.get(object.channel);
 	}
+	editedValues.channel = channel ? channel : message.channel;
 
-	await channel.send({ embed }).catch(() => {
-		throw ErrorMessages.unableToSendMessage;
-	});
-
-	return true;
-}
+	return editedValues;
+};
 
 module.exports = {
-	send
+	send: async (object, message, mentions) => {
+		const { title, color, footer, channel } = custom(object, message);
+
+		const embed = new Discord.MessageEmbed()
+			.setTitle(title)
+			.setAuthor(`${message.member.user.tag}`, `${message.member.user.displayAvatarURL()}`)
+			.setColor(color)
+			.addFields(
+				{ name: `**Channel:**`, value: `${message.channel}`, inline: true },
+				{ name: `**Mentions:**`, value: `${mentions}`, inline: true }
+			)
+			.setFooter(footer)
+			.setTimestamp();
+
+		if(ignore(object, message) == false) {
+			return false;
+		}
+
+		await channel.send({ embeds: [embed] }).catch(() => {
+			throw ErrorMessages.unableToSendMessage;
+		});
+
+		return true;
+
+	}
 };
