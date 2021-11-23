8709385415f5212d2c2988c91235435b6e0561a7
RaZe <75721601+RaZeSloth@users.noreply.github.com>
2021-11-23 17:38:39 +0200

    Implemented function overloads

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,5 @@
 declare module "discord.js-ghost-ping" {
-    export function detector(event: 'messageDelete' | 'messageUpdate', message: object, newMessage?: object): Promise<void>;
+    export function detector(event: 'messageDelete', message: object): Promise<void>;
+    export function detector(event: 'messageUpdate', message: object, newMessage: object): Promise<void>;
+    export function detector(event: 'messageDelete' | 'messageUpdate', message: object, newMessage?: object): Promise<void>
 }
