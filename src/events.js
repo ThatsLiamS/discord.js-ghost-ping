7b478db5fa0feb4e732479c4afe398a2ac8d262a
Liam Skinner <liamdavidskinner@gmail.com>
2022-02-05 11:59:18 +0000

    fix: removed breaking error

--- src/events.js
+++ src/events.js
@@ -1,8 +1,11 @@
 const send = require('./util/send.js');
 
 const validate = (member, message) => {
-	if (!member.user.bot && member.id != message.author.id) return member;
+	if (!member.user.bot && member.id != message.author.id) return member.toString();
 };
+const filter = (r) => {
+	if (r.toString().startsWith('<')) return r.toString();
+}
 
 /**
  * Handles the messageUpdate event
@@ -19,10 +22,10 @@ const messageUpdate = async (oldMessage, newMessage, object) => {
 	if (oldMessage.author.bot || oldMessage.mentions.members.size === 0 && oldMessage.mentions.roles.size === 0) return false;
 
 	let oldArray = oldMessage.mentions.members.map(member => validate(member, newMessage));
-	oldArray = oldMessage.mentions.roles.filter((r) => r.toString().startsWith('<')).concat(oldArray);
+	oldArray = [...oldMessage.mentions.roles.map(x => filter(x)), ...oldArray];
 
 	let newArray = newMessage.mentions.members.map(member => validate(member, newMessage));
-	newArray = newMessage.mentions.roles.filter((r) => r.toString().startsWith('<')).concat(newArray);
+	newArray = [...newMessage.mentions.roles.map(x => filter(x)), ...newArray];
 
 	const mentions = oldArray.filter((mention) => !newArray.includes(mention));
 
@@ -45,7 +48,7 @@ const messageDelete = async (message, object) => {
 	if (message.author.bot || message.mentions.members.size == 0 && message.mentions.roles.size == 0) return false;
 
 	let mentions = message.mentions.members.map(member => validate(member, message));
-	mentions = message.mentions.roles.filter((r) => r.toString().startsWith('<')).concat(mentions);
+	mentions = [...message.mentions.roles.map(x => filter(x)), ...mentions];
 
 	if (mentions.length < 1) return false;
 	return await send(object, message, mentions);
